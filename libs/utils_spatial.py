1824785a28bedb5bc021c610db00b4d75485f82d
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-05-14 18:03:24 +0200

    [+] Clean first part

--- libs/utils_spatial.py
+++ libs/utils_spatial.py
@@ -8,26 +8,25 @@
 #                                                       +++##+++::::::::::::::       +#+    +:+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       +#+    +#+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       #+#    #+#     #+#     #+#    #+#      #
-#      Update: 2022/04/14 15:41:02 by branlyst and ismai  ::::::::::::::::::::        ########      ###      ######## .fr   #
+#      Update: 2022/05/14 17:48:56 by branlyst and ismai  ::::::::::::::::::::        ########      ###      ######## .fr   #
 #                                                                                                                           #
 # ************************************************************************************************************************* #
 
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import contextily as ctx
 
-def visualise_stations(stations, borough_file):
+def visualise_stations(stations):
     """
         Generates a figure with geospatial data
         Shows the stations position with their ID
         Boroughs are used as a background layer
     """
-    borough = gpd.read_file(borough_file)
-    fig = plt.figure(figsize=(20,20))
-    gdf = gpd.GeoDataFrame(stations, geometry=gpd.points_from_xy(stations.longitude, stations.latitude))
-    ax = borough.plot(alpha=0.5, edgecolor='k', color='#DDD')
-    gdf.plot(ax=ax, column='nom', markersize=20)
+    gdf = gpd.GeoDataFrame(stations, geometry=gpd.points_from_xy(stations.longitude, stations.latitude), crs='EPSG:4326').to_crs(epsg=3857)
+    ax = gdf.plot(column='nom', markersize=20)
     ax.set_xlabel('Longitude', fontsize=10)
-    ax.set_ylabel('Latitude', fontsize='medium')
-    ax.set_title(f"Stations")
+    ax.set_ylabel('Latitude', fontsize=10)
+    ax.set_title(f"Visualisation of the stations")
+    ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron)
     for x, y, label in zip(gdf.geometry.x, gdf.geometry.y, gdf.numero_station):
-        ax.annotate(label, xy=(x, y), xytext=(3, 3), textcoords="offset points")
+        ax.annotate(label, xy=(x, y), xytext=(1, 4), textcoords="offset points")
