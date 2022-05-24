5faeb639076e780907a3ab519e8a3bb5bc6a26f5
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-05-24 15:42:03 +0200

    [+] Docstring for PearsonCorrelation and TemplateMethod

--- src/FeatureSelectionMethods/PearsonCorrelation.py
+++ src/FeatureSelectionMethods/PearsonCorrelation.py
@@ -8,13 +8,17 @@
 #                                                       +++##+++::::::::::::::       +#+    +:+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       +#+    +#+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       #+#    #+#     #+#     #+#    #+#      #
-#      Update: 2022/05/14 14:18:07 by branlyst and ismai  ::::::::::::::::::::        ########      ###      ######## .fr   #
+#      Update: 2022/05/24 15:30:29 by branlyst and ismai  ::::::::::::::::::::        ########      ###      ######## .fr   #
 #                                                                                                                           #
 # ************************************************************************************************************************* #
 
 from src.FeatureSelectionMethods.TemplateMethod import TemplateMethod
 
 class PearsonCorrelation(TemplateMethod):
+    """
+        PearsonCorrelation is a class which implements the TemplateMethods in order to implement the Pearson Correlation feature selection
+    """
+
     def __init__(self):
         TemplateMethod.__init__(self, 'PearsonCorrelation')
 
