fa96b03b985688577ffaac2475a508dda716dc8f
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-05-24 16:20:06 +0200

    [~] Run black

--- src/FeatureSelectionMethods/PearsonCorrelation.py
+++ src/FeatureSelectionMethods/PearsonCorrelation.py
@@ -14,13 +14,14 @@
 
 from src.FeatureSelectionMethods.TemplateMethod import TemplateMethod
 
+
 class PearsonCorrelation(TemplateMethod):
     """
-        PearsonCorrelation is a class which implements the TemplateMethods in order to implement the Pearson Correlation feature selection
+    PearsonCorrelation is a class which implements the TemplateMethods in order to implement the Pearson Correlation feature selection
     """
 
     def __init__(self):
-        TemplateMethod.__init__(self, 'PearsonCorrelation')
+        TemplateMethod.__init__(self, "PearsonCorrelation")
 
     def select(self, dataframe, target_columns):
         target_correlation = dataframe.corr()[target_columns]
--- src/FeatureSelectionMethods/TemplateMethod.py
+++ src/FeatureSelectionMethods/TemplateMethod.py
@@ -12,17 +12,19 @@
 #                                                                                                                           #
 # ************************************************************************************************************************* #
 
+
 class TemplateMethod:
     """
-        The class TemplateMethod provide a Template which can be implemented or extended to implement methods for Feature Selection.
+    The class TemplateMethod provide a Template which can be implemented or extended to implement methods for Feature Selection.
 
-        Args:
-            method_name (str) : the name of the implemented method, name used to find the right instance
+    Args:
+        method_name (str) : the name of the implemented method, name used to find the right instance
 
-        Attributes:
-            _score (DataFrame) : Dataframe which contains len(target_columns) columns and len(features) lines representing the score result of the feature selection
-            _method_name (str) : variable which stores the method name
+    Attributes:
+        _score (DataFrame) : Dataframe which contains len(target_columns) columns and len(features) lines representing the score result of the feature selection
+        _method_name (str) : variable which stores the method name
     """
+
     _score = None
     _method_name = None
 
@@ -31,22 +33,22 @@ class TemplateMethod:
 
     def select(self, dataframe, target_columns):
         """
-            Select abstract method. Must be implemented. 
+        Select abstract method. Must be implemented.
 
-            Args:
-                dataframe (DataFrame) : dataframe which contains the data used to apply the feature selection. 1 column by feature and 1 line by entry
-                target_columns (str[]) : array of the target column names used to apply the feature selection
+        Args:
+            dataframe (DataFrame) : dataframe which contains the data used to apply the feature selection. 1 column by feature and 1 line by entry
+            target_columns (str[]) : array of the target column names used to apply the feature selection
         """
         raise NotImplementedError
 
     def get_features_importance(self):
         """
-            Accessor to the _score variable
+        Accessor to the _score variable
         """
         return self._score
 
     def get_method_name(self):
         """
-            Accessor to the _method_name variable
+        Accessor to the _method_name variable
         """
         return self._method_name
